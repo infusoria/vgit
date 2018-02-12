0d6c981b518a5d5c2e4f334936a04e62be1e676e
Igor <36212975+infusoria@users.noreply.github.com>
2018-02-12 01:11:51 -0800

    2 urok dz 2

--- features/area_screen.feature
+++ features/area_screen.feature
@@ -1,29 +1,45 @@
 @area
 Feature: User is able to convert area units
 
-  @wip
-  Scenario: User is able to dismiss help
-    Given I land on help popup
-    When I click on GOT IT button
-    Then I land on Area screen
+
+  Background:
+    Given I click on GOT IT button
+    And I land on "Area" screen
+
+
+#  Scenario: User is able to dismiss help
+#    Given I land on Help popup
+#    When I click on GOT IT button
+#    Then I land on Arean screen
 
 
   Scenario: User  is able to swap values
-    Given I click on GOT IT button
-    And I see "Sq Kilometre" in FROM header
+    Given I see "Sq Kilometre" in FROM header
     And I see "Sq Metre" in TO header
     When I click on SWAP button
     Then I see "Sq Metre" in FROM header
     And I see "Sq Kilometre" in TO header
 
+
   Scenario Outline: User is  able to convert default units
-    Given I click on GOT IT button
-    And I click on CLEAR button
+    Given I click on CLEAR button
     When I enter "<target>" to FROM field
     Then I get "<result>" in TO field
-
     Examples:
     |target|result|
     |1     |1000000|
     |2     |2000000|
     |3     |3000000|
+
+  @wip
+  Scenario: User is able to use soft keyboard to enter values
+    Given I click on CLEAR button
+    When I click on FROM field
+    And I press "12" on soft keyboard
+    Then I get "12000000" in TO field
+
+
+  Scenario: User is able to work with Hectares
+    When I select "Hectare" from left column
+    Then I see "Hectare" in FROM header
+    And I get "10000" in TO field
--- features/step_definitions/area_screen_steps.rb
+++ features/step_definitions/area_screen_steps.rb
@@ -1,14 +1,19 @@
-Given(/^I land on help popup$/) do
-  #puts ("user lands on popup help")
-  text("Help")
+Given(/^I land on "([^"]*)" popup$/) do |value|
+  actual_value = find_element(xpath: "//android.widget.LinearLayout[@text='#{value}']").text
+  if actual_value != value
+    fail("Expected value is #{value}, but actual value was #{actual_value}")
+  end
 end
 
 When(/^I click on GOT IT button$/) do
   find_element(id: "button1").click
 end
 
-Then(/^I land on Area screen$/) do
-  text("Area")
+Then(/^I land on "([^"]*)" screen$/) do |value|
+  actual_value = find_element(id: "toolbar").find_element(xpath: "//android.widget.TextView[@text='#{value}']").text
+  if actual_value != value
+    fail("Expected value is #{value}, but actual value was #{actual_value}")
+  end
 end
 
 When(/^I click on SWAP button$/) do
@@ -17,25 +22,60 @@ end
 
 Then(/^I see "([^"]*)" in FROM header$/) do |value|
  actual_value = find_element(id: "header_text_unit_from").text
- puts("Expected value is #{value}")
- puts("Actual value is #{actual_value}")
+ if actual_value != value
+   fail("Expected value is #{value}, but actual value was #{actual_value}")
+ end
+ # puts("Expected value is #{value}")
+ # puts("Actual value is #{actual_value}")
 
 end
 
 And(/^I see "([^"]*)" in TO header$/) do |value|
   actual_value = find_element(id: "header_text_unit_to").text
-  puts("Expected value is #{value}")
-  puts("Actual value is #{actual_value}")
+  if actual_value != value
+    fail("Expected value is #{value}, but actual value was #{actual_value}")
+  end
 end
 
 And(/^I click on CLEAR button$/) do
-  puts("User clicks on CLEAR button")
+  find_element(id: "menu_clear").click
 end
 
 When(/^I enter "([^"]*)" to FROM field$/) do |value|
-  puts("User entered value #{value}")
+  find_element(id: "header_value_from").send_keys(value)
+  actual_value = find_element(id: "header_value_from").text
+  if actual_value != value
+    fail("Expected value is #{value}, but actual value was #{actual_value}")
+  end
 end
 
 Then(/^I get "([^"]*)" in TO field$/) do |value|
-  puts("User sees #{value} in result field")
+  actual_value = find_element(id: "header_value_to").text
+  if actual_value != value
+    fail("Expected value is #{value}, but actual value was #{actual_value}")
+  end
+end
+
+When(/^I click on FROM field$/) do
+  find_element(id: "header_value_from").click
+end
+
+And(/^I press "([^"]*)" on soft keyboard$/) do |value|
+  digits= value.split("")
+  digits.each do |key|
+    digit = Integer(key)
+    press_keycode 7 + digit
+  end
+  actual_value = find_element(id: "header_value_from").text
+  if actual_value != value
+    fail("Expected value is #{value}, but actual value was #{actual_value}")
+  end
+end
+
+When(/^I select "([^"]*)" from left column$/) do |value|
+  find_element(id: "radio_group_from").find_element(xpath: "//android.widget.RadioButton[@text='#{value}']").click
+  actual_value = find_element(id: "radio_group_from").find_element(xpath: "//android.widget.RadioButton[@text='#{value}']").text
+  if actual_value != value
+    fail("Expected value is #{value}, but actual value was #{actual_value}")
+  end
 end
